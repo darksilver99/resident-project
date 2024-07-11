ad121d89acf14e4a36a728a0009fd91b9c4e1654
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-07-11 03:37:28 +0000

    Updating to latest FlutterFlow output.

--- lib/landing_view/home_page/home_page_widget.dart
+++ lib/landing_view/home_page/home_page_widget.dart
@@ -1,4 +1,6 @@
+import '/auth/firebase_auth/auth_util.dart';
 import '/backend/backend.dart';
+import '/component/custom_info_alert_view/custom_info_alert_view_widget.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
@@ -7,6 +9,7 @@ import 'package:flutter/scheduler.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
+import 'package:webviewx_plus/webviewx_plus.dart';
 import 'home_page_model.dart';
 export 'home_page_model.dart';
 
@@ -29,9 +32,39 @@ class _HomePageWidgetState extends State<HomePageWidget> {
 
     // On page load action.
     SchedulerBinding.instance.addPostFrameCallback((_) async {
-      if (FFAppState().currentProjectData.name == null ||
-          FFAppState().currentProjectData.name == '') {
-        context.goNamed('SelectProjectPage');
+      if (valueOrDefault(currentUserDocument?.type, '') == 'resident') {
+        if (FFAppState().currentProjectData.name == null ||
+            FFAppState().currentProjectData.name == '') {
+          context.goNamedAuth('SelectProjectPage', context.mounted);
+        }
+      } else {
+        await showDialog(
+          context: context,
+          builder: (dialogContext) {
+            return Dialog(
+              elevation: 0,
+              insetPadding: EdgeInsets.zero,
+              backgroundColor: Colors.transparent,
+              alignment: AlignmentDirectional(0.0, 0.0)
+                  .resolve(Directionality.of(context)),
+              child: WebViewAware(
+                child: GestureDetector(
+                  onTap: () => _model.unfocusNode.canRequestFocus
+                      ? FocusScope.of(context).requestFocus(_model.unfocusNode)
+                      : FocusScope.of(context).unfocus(),
+                  child: CustomInfoAlertViewWidget(
+                    title: 'ขออภัยผู้ใช้นี้ไม่สามารถใช้งานระบบนี้ได้',
+                    detail: 'กรุณาติดต่อผู้ดูแลระบบเพื่อสอบถามปัญหา',
+                  ),
+                ),
+              ),
+            );
+          },
+        ).then((value) => setState(() {}));
+
+        GoRouter.of(context).prepareAuthEvent();
+        await authManager.signOut();
+        GoRouter.of(context).clearRedirectLocation();
       }
     });
   }
@@ -47,127 +80,130 @@ class _HomePageWidgetState extends State<HomePageWidget> {
   Widget build(BuildContext context) {
     context.watch<FFAppState>();
 
-    return GestureDetector(
-      onTap: () => _model.unfocusNode.canRequestFocus
-          ? FocusScope.of(context).requestFocus(_model.unfocusNode)
-          : FocusScope.of(context).unfocus(),
-      child: Scaffold(
-        key: scaffoldKey,
-        backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
-        body: Padding(
-          padding: EdgeInsetsDirectional.fromSTEB(1.0, 0.0, 0.0, 0.0),
-          child: Container(
-            width: double.infinity,
-            height: double.infinity,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: Image.asset(
-                  'assets/images/simple-blank-orange-background-vector-business_53876-175746.jpg',
-                ).image,
-              ),
-            ),
-            child: Padding(
-              padding: EdgeInsetsDirectional.fromSTEB(8.0, 0.0, 8.0, 0.0),
-              child: StreamBuilder<List<ResidentServiceListRecord>>(
-                stream: queryResidentServiceListRecord(
-                  parent: FFAppState().currentProjectData.projectRef,
-                  queryBuilder: (residentServiceListRecord) =>
-                      residentServiceListRecord
-                          .where(
-                            'status',
-                            isEqualTo: 1,
-                          )
-                          .orderBy('seq'),
+    return Builder(
+      builder: (context) => GestureDetector(
+        onTap: () => _model.unfocusNode.canRequestFocus
+            ? FocusScope.of(context).requestFocus(_model.unfocusNode)
+            : FocusScope.of(context).unfocus(),
+        child: Scaffold(
+          key: scaffoldKey,
+          backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+          body: Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(1.0, 0.0, 0.0, 0.0),
+            child: Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: Image.asset(
+                    'assets/images/simple-blank-orange-background-vector-business_53876-175746.jpg',
+                  ).image,
                 ),
-                builder: (context, snapshot) {
-                  // Customize what your widget looks like when it's loading.
-                  if (!snapshot.hasData) {
-                    return Center(
-                      child: SizedBox(
-                        width: 50.0,
-                        height: 50.0,
-                        child: CircularProgressIndicator(
-                          valueColor: AlwaysStoppedAnimation<Color>(
-                            FlutterFlowTheme.of(context).primary,
-                          ),
-                        ),
-                      ),
-                    );
-                  }
-                  List<ResidentServiceListRecord>
-                      gridViewResidentServiceListRecordList = snapshot.data!;
-
-                  return GridView.builder(
-                    padding: EdgeInsets.fromLTRB(
-                      0,
-                      64.0,
-                      0,
-                      16.0,
-                    ),
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 3,
-                      crossAxisSpacing: 8.0,
-                      mainAxisSpacing: 8.0,
-                      childAspectRatio: 1.0,
-                    ),
-                    scrollDirection: Axis.vertical,
-                    itemCount: gridViewResidentServiceListRecordList.length,
-                    itemBuilder: (context, gridViewIndex) {
-                      final gridViewResidentServiceListRecord =
-                          gridViewResidentServiceListRecordList[gridViewIndex];
-                      return Material(
-                        color: Colors.transparent,
-                        elevation: 3.0,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8.0),
-                        ),
-                        child: Container(
-                          width: 100.0,
-                          height: 100.0,
-                          decoration: BoxDecoration(
-                            color: FlutterFlowTheme.of(context)
-                                .secondaryBackground,
-                            borderRadius: BorderRadius.circular(8.0),
-                          ),
-                          child: Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Column(
-                              mainAxisSize: MainAxisSize.max,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                ClipRRect(
-                                  borderRadius: BorderRadius.circular(8.0),
-                                  child: Image.network(
-                                    gridViewResidentServiceListRecord.icon,
-                                    width: 56.0,
-                                    height: 56.0,
-                                    fit: BoxFit.cover,
-                                  ),
-                                ),
-                                Expanded(
-                                  child: Text(
-                                    gridViewResidentServiceListRecord.subject,
-                                    textAlign: TextAlign.center,
-                                    maxLines: 2,
-                                    style: FlutterFlowTheme.of(context)
-                                        .bodyMedium
-                                        .override(
-                                          fontFamily: 'Inter',
-                                          fontSize: 18.0,
-                                          letterSpacing: 0.0,
-                                          fontWeight: FontWeight.normal,
-                                        ),
-                                  ),
-                                ),
-                              ],
+              ),
+              child: Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(8.0, 0.0, 8.0, 0.0),
+                child: StreamBuilder<List<ResidentServiceListRecord>>(
+                  stream: queryResidentServiceListRecord(
+                    parent: FFAppState().currentProjectData.projectRef,
+                    queryBuilder: (residentServiceListRecord) =>
+                        residentServiceListRecord
+                            .where(
+                              'status',
+                              isEqualTo: 1,
+                            )
+                            .orderBy('seq'),
+                  ),
+                  builder: (context, snapshot) {
+                    // Customize what your widget looks like when it's loading.
+                    if (!snapshot.hasData) {
+                      return Center(
+                        child: SizedBox(
+                          width: 50.0,
+                          height: 50.0,
+                          child: CircularProgressIndicator(
+                            valueColor: AlwaysStoppedAnimation<Color>(
+                              FlutterFlowTheme.of(context).primary,
                             ),
                           ),
                         ),
                       );
-                    },
-                  );
-                },
+                    }
+                    List<ResidentServiceListRecord>
+                        gridViewResidentServiceListRecordList = snapshot.data!;
+
+                    return GridView.builder(
+                      padding: EdgeInsets.fromLTRB(
+                        0,
+                        64.0,
+                        0,
+                        16.0,
+                      ),
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3,
+                        crossAxisSpacing: 8.0,
+                        mainAxisSpacing: 8.0,
+                        childAspectRatio: 1.0,
+                      ),
+                      scrollDirection: Axis.vertical,
+                      itemCount: gridViewResidentServiceListRecordList.length,
+                      itemBuilder: (context, gridViewIndex) {
+                        final gridViewResidentServiceListRecord =
+                            gridViewResidentServiceListRecordList[
+                                gridViewIndex];
+                        return Material(
+                          color: Colors.transparent,
+                          elevation: 3.0,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          child: Container(
+                            width: 100.0,
+                            height: 100.0,
+                            decoration: BoxDecoration(
+                              color: FlutterFlowTheme.of(context)
+                                  .secondaryBackground,
+                              borderRadius: BorderRadius.circular(8.0),
+                            ),
+                            child: Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.max,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  ClipRRect(
+                                    borderRadius: BorderRadius.circular(8.0),
+                                    child: Image.network(
+                                      gridViewResidentServiceListRecord.icon,
+                                      width: 56.0,
+                                      height: 56.0,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Text(
+                                      gridViewResidentServiceListRecord.subject,
+                                      textAlign: TextAlign.center,
+                                      maxLines: 2,
+                                      style: FlutterFlowTheme.of(context)
+                                          .bodyMedium
+                                          .override(
+                                            fontFamily: 'Inter',
+                                            fontSize: 18.0,
+                                            letterSpacing: 0.0,
+                                            fontWeight: FontWeight.normal,
+                                          ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
               ),
             ),
           ),
