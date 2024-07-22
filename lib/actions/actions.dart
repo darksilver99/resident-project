53884b0ec177508ea81675aab97a54de7f6d8feb
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-07-22 12:30:24 +0000

    Updating to latest FlutterFlow output.

--- lib/actions/actions.dart
+++ lib/actions/actions.dart
@@ -3,6 +3,7 @@ import '/backend/backend.dart';
 import '/backend/schema/structs/index.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
+import '/custom_code/actions/index.dart' as actions;
 import '/flutter_flow/custom_functions.dart' as functions;
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -35,6 +36,7 @@ Future setCurrentProjectData(
 }
 
 Future clearPrefData(BuildContext context) async {
+  await actions.unSubscript();
   FFAppState().configData = ConfigDataStruct();
   FFAppState().currentProjectData = ProjectDataStruct();
   FFAppState().currentResidentData = ResidentDataStruct();
